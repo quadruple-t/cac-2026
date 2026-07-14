a86586f1cdd04aaaf899b3a779a78b822e8b016d
Mukiewukie <16mukeshr@gmail.com>
2026-07-14 12:57:55 -0400

    fixed issues?

--- components/features/index.ts
+++ components/features/index.ts
@@ -1,6 +1,4 @@
-// Core 5 Features of Aid Compass
-export { default as SituationIntake } from './situation-intake';
+// Core Features of Aid Compass
 export { default as AidDashboard } from './aid-dashboard';
-export { default as DocumentChecklist } from './document-checklist';
 export { default as DeadlineTracker } from './deadline-tracker';
 export { default as FemaExplainer } from './fema-explainer';
